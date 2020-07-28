4f47df2ccda85e559317ae461b022c057e666668
sid <68276495+siddant-rachha@users.noreply.github.com>
2020-07-29 01:45:01 +0530

    Add files via upload

--- resources/js/getStatebyIP.js
+++ resources/js/getStatebyIP.js
@@ -1,10 +1,17 @@
 let shortStateCode;
 
 window.addEventListener("load", () => {
-    let state_code = geoplugin_regionCode()
-    let _country = geoplugin_countryName()
-
+    let state_code;
+    let _country;
     let user_state;
+    try {
+        state_code = geoplugin_regionCode()
+        _country = geoplugin_countryName()
+    } catch (error) {
+        user_state = 'Delhi';
+        state_code = 'DL';
+    }
+
     if (_country == 'India') {
         state_list.forEach(state => {
             if (state.code == state_code) {
