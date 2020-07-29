6ca891145010f22cb7897c3a8343f4e987016fa7
sid <68276495+siddant-rachha@users.noreply.github.com>
2020-07-30 01:51:54 +0530

    Add files via upload

--- resources/js/liveStateData.js
+++ resources/js/liveStateData.js
@@ -1,11 +1,35 @@
 const state_name_element = document.querySelector(".state .name")
 const total_cases_element = document.querySelector(".total-cases .value")
 const total_tested_element = document.querySelector(".total-tested .value")
-// const new_cases_element = document.querySelector(".total-cases .new-value")
 const recovered_element = document.querySelector(".recovered .value")
-// const new_recovered_element = document.querySelector(".recovered .new-value")
 const deaths_element = document.querySelector(".deaths .value")
-// const new_deaths_element = document.querySelector(".deaths .new-value")
+const india_cases = document.querySelector(".india-cases .india-value")
+const india_recovered = document.querySelector(".india-recovered .india-value")
+const india_deaths = document.querySelector(".india-deaths .india-value")
+const india_tested = document.querySelector(".india-tested .india-value")
+
+
+fetch("https://covid-19-data.p.rapidapi.com/country?format=json&name=india", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+		"x-rapidapi-key": "4db143a0d2msh5c29b3c778a0ca5p1a0766jsn8c479a5cfe1f"
+	}
+})
+.then(response => {
+	return response.json();
+})
+.then(response=>{
+    india_cases.textContent = response[0].confirmed.toLocaleString()
+    india_recovered.textContent = response[0].recovered.toLocaleString()
+    india_deaths.textContent = response[0].deaths.toLocaleString()
+    console.log(response)
+})
+.catch(err => {
+	console.log(err);
+});
+
+
 let stateliveData;
 
 
@@ -18,7 +42,7 @@ liveStateData = (shortStateCode) => {
         .then(response => {
             console.log(shortStateCode)
             stateliveData = response[shortStateCode.toUpperCase()]['total']
-            console.log(stateliveData)
+            console.log(response)
         })
         .then(response=>{
             total_cases_element.textContent = stateliveData.confirmed.toLocaleString()
